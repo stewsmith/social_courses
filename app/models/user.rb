b0fc4f17b42f15af9377b804c09e3d3487309648
Kaushal Parikh <kaushalp88@gmail.com>
2014-02-22 18:45:58 -0500

    fixed facebook arror

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
     def self.from_omniauth(auth)
-        where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-            user.provider = auth.provider
+        where(auth.slice(:uid)).first_or_initialize.tap do |user|
             user.uid = auth.uid
             user.name = auth.info.name
             user.oauth_token = auth.credentials.token
