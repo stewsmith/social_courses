7c64baba5b09326eb180f1109536edb9bf8849ce
Kaushal Parikh <kaushalp88@gmail.com>
2014-02-23 01:17:24 -0500

    not logged in error fixed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,14 @@
 require 'fb_graph'
 class StaticPagesController < ApplicationController
     def index
-        fb_user = FbGraph::User.new('me', :access_token => current_user.oauth_token)
+        if current_user
+            fb_user = FbGraph::User.new('me', :access_token => current_user.oauth_token)
+            fb_user.fetch()
 
-        fb_user.fetch()
+            @friends = fb_user.friends()
+            @picture = fb_user.picture(:access_token => current_user.oauth_token, :type => 'large')
 
-        @friends = fb_user.friends()
-        @picture = fb_user.picture(:access_token => current_user.oauth_token, :type => 'large')
-
-        puts @picture
+            puts @picture
+        end
     end
 end
